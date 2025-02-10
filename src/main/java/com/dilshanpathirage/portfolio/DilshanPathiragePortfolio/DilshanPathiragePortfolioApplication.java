f0079d96f2ad3e7950e307fc388ef76260a0e3b4
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-02-10 16:13:37 +0530

    create

---
+++ src/main/java/com/dilshanpathirage/portfolio/DilshanPathiragePortfolio/DilshanPathiragePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.dilshanpathirage.portfolio.DilshanPathiragePortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DilshanPathiragePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DilshanPathiragePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilshanpathirage/portfolio/DilshanPathiragePortfolio/DilshanPathiragePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilshanpathirage.portfolio.DilshanPathiragePortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DilshanPathiragePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
